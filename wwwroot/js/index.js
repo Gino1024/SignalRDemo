7d5289494b9cb16406e7a4cf0daa59add34704d2
Gino <ginoweng1024@gmail.com>
2022-06-01 23:45:43 +0800

    feat: 登入後隱藏登入畫面

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,5 +1,40 @@
 import { Guest } from './model.js';
 
-let guest = new Guest('gino');
+//data
+let guest = new Guest();
+let tmpGuest = new Guest();
+let isLogin = false;
+//methods
+$('#login').click(()=>{
+    let msg = CheckForm();
 
-console.log(guest);
+    if(msg.length > 0){
+        console.log(msg);
+        alert(msg);
+        return;
+    }
+
+    tmpGuest.name = $('#name').val();
+    guest = JSON.parse(JSON.stringify(tmpGuest));
+
+    isLogin = true;
+    ChangeLoginDisplay();
+})
+
+function CheckForm(){
+    let name = $('#name').val();
+    if(name.length == 0){
+        return "加入失敗: 請輸入姓名!";
+    }
+    return "";   
+}
+
+function ChangeLoginDisplay(){
+    let pannel = $('.login-background');
+    console.log(pannel);
+    if(isLogin){
+        pannel.addClass('noDisplay');
+    }else{
+        pannel.removeClass('noDisplay');
+    }
+}
